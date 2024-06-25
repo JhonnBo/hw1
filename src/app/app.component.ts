cf71e279d2ab41633eba448b11869b8a28836d3c
JhonnBo <JhonnBo@gmail.com>
2024-06-25 19:29:58 +0300

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PhotoComponent } from './photo/photo.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { KnowledgeComponent } from './knowledge/knowledge.component';
+import { AboutComponent } from './about/about.component';
+import { LinksComponent } from './links/links.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PhotoComponent, JobsComponent, KnowledgeComponent, AboutComponent, LinksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
